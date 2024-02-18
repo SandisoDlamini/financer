546880a69b6b932376d49395be3d1256880c8757
SandisoDlamini <sihlegamer65@gmail.com>
2024-02-18 17:53:10 +0200

    added file creation capability

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,80 @@
 use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+use std::{fs, path::Path};
 
 const SAVINGS_PERCENTAGE: f64 = 0.5;
 const SPENDING_PERCENTAGE: f64 = 0.3;
 const SURPLUS_PERCENTAGE: f64 = 0.2;
 
+fn create_file() -> String {
+    loop {
+        
+        let mut file_name = String::new();
+
+        println!("Please input name of month to save to:");
+
+        io::stdin()
+            .read_line(&mut file_name)
+            .expect("Failed to read input");
+
+        let file_name = file_name.trim().to_string();
+
+        println!("Saving to: {}", &file_name);
+
+
+        let mut choice_confirmation = String::new();
+
+        println!("Are you sure you want to save to {}? [y/N]", file_name);
+
+        io::stdin()
+            .read_line(&mut choice_confirmation)
+            .expect("Cannot save to file.");
+        
+        let choice_confirmation = choice_confirmation.trim();
+
+        let choices_for = ["Y", "y", "YES", "yes"];
+        let choices_against = ["N", "n", "NO", "no"];
+
+        if choices_for.iter().any(|&i| i == choice_confirmation) {
+            break file_name;
+        }
+
+        else if choices_against.iter().any(|&i| i == choice_confirmation) {
+            continue;
+        }
+
+        else {
+            continue;
+        }
+    }
+
+}
+
+fn open_file(filename: String) {
+
+    let file_name = &filename;
+    
+    let directory_name_string = String::from("./src/month_files/");
+    
+    let file_name_added = directory_name_string + file_name + ".txt";
+
+    let file_name_string = file_name_added.as_str();
+
+    let file_path = fs::canonicalize(file_name_string).expect("Cannot get path");
+  
+    assert!(Path::new("./month_files/").is_relative());
+    println!("{}", format!("{:?}",file_path));
+
+    let mut file = File::create(file_path).expect("Failed to create file");
+    let amount = money_divider(user_input());
+
+    for i in amount.into_iter() {
+        println!("{}", format!("{:.2}", &i));
+        let j = format!("{:.2}", i);
+        file.write(j.as_bytes()).expect("Failed to write to file");
+    }
+}
 
 fn user_input() -> f64 {
     
@@ -29,20 +100,25 @@ fn user_input() -> f64 {
     }
 }
 
-fn money_divider(a: f64) {
+fn money_divider(a: f64) -> Vec<f64> {
 
     let gross_income = &a;
     let savings = &*gross_income * SAVINGS_PERCENTAGE;
     let spending_budget = &*gross_income * SPENDING_PERCENTAGE;
     let surplus_budget = &*gross_income * SURPLUS_PERCENTAGE;
 
-    println!("{}",format!("Your necessary savings are: E {:.2}", savings));
-    println!("{}",format!("Your spending budget is:    E {:.2}", spending_budget));
-    println!("{}",format!("Your surplus/fun budget is: E {:.2}", surplus_budget));
+    println!("{}",format!("Your necessary savings are: E {:.2}", &savings));
+    println!("{}",format!("Your spending budget is:    E {:.2}", &spending_budget));
+    println!("{}",format!("Your surplus/fun budget is: E {:.2}", &surplus_budget));
+
+    let expenditure = vec![savings, spending_budget, surplus_budget];
+
+    return expenditure
 
 }
 
+
 fn main() {
     println!("Hello, User!");
-    money_divider(user_input());
+    open_file(create_file());
 }
